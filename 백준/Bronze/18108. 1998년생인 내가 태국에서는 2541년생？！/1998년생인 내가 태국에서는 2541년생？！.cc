32079bd4e5b2e840c25507310c4f21fe741e7adf
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-01-17 09:07:37 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int bu_year;
+    int AD_year;
+    
+    scanf("%d", &bu_year);
+    
+    AD_year = bu_year - 543;
+    
+    printf("%d", AD_year);
+    return 0;
+}
