701bd2ef2e745160c4885be8e5ddee53aff520fb
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-01-31 15:15:54 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,17 +2,19 @@ import sys
 
 N = int(sys.stdin.readline())
 
-ans = [[0 for _ in range(3)] for _ in range(N)]
+ans = [0 for _ in range(3)]
+
 
 for i in range(N):
   RGB = list(map(int,sys.stdin.readline().split()))
   if i == 0:
-    ans[i][0] = RGB[0]
-    ans[i][1] = RGB[1]
-    ans[i][2] = RGB[2]
-  else:
-    ans[i][0] = min(ans[i-1][1],ans[i-1][2]) + RGB[0]
-    ans[i][1] = min(ans[i-1][0],ans[i-1][2]) + RGB[1]
-    ans[i][2] = min(ans[i-1][0],ans[i-1][1]) + RGB[2]
-    
-print(min(ans[N-1]))
+    ans[0] = RGB[0]
+    ans[1] = RGB[1]
+    ans[2] = RGB[2]
+    continue
+
+  R = (min(ans[1], ans[2]) + RGB[0])
+  G = (min(ans[0], ans[2]) + RGB[1])
+  B = (min(ans[0], ans[1]) + RGB[2])
+  ans[0], ans[1], ans[2] = R, G, B
+print(min(ans))
