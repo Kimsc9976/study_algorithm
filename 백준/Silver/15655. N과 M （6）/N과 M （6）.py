258b003fe497c85e92dfdb97abcc3527974bf1f9
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-02-01 09:52:40 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,24 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split()) 
+num_lst = sorted(list(map(int, sys.stdin.readline().split())))
+
+lst = num_lst.copy()
+
+def NM(target):
+  
+  is_used = list()
+  def bt(depth,is_used):
+    if len(is_used) == M:
+      print(*is_used)
+      return
+    
+    for i in range(depth, len(target)):
+      if target[i] in is_used:
+        continue
+      is_used.append(target[i])
+      bt(i,is_used)
+      is_used.pop()
+  bt(0,is_used)
+
+NM(lst)
