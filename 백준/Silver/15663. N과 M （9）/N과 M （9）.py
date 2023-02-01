7287583501bf6e8510a4c141d3b410d4dfa82eb3
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-02-01 17:20:26 +0900

    [Silver II] Title: N과 M (9), Time: 264 ms, Memory: 120664 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,30 @@
+import sys
+N, M = map(int, sys.stdin.readline().split()) 
+num_lst = sorted(list(map(int, sys.stdin.readline().split())))
+
+def NM(target):
+  check = set()
+  is_used = list()
+  is_true = [True for _ in range(len(target))]
+  
+  def bt(depth, is_used : list):
+    nonlocal check, is_true
+    if len(is_used) == M:
+      if ' '.join(map(str,is_used)) in check:
+        return
+      check.add(' '.join(map(str,is_used)))
+      print(' '.join(map(str,is_used)))
+      return
+    
+    for i in range(len(target)):
+      if (is_true[i] == False):
+        continue
+      is_true[i] = False
+      is_used.append(target[i])
+      bt(depth + 1, is_used)
+      is_true[i] = True
+      is_used.pop()
+
+  bt(0, is_used)
+  
+NM(num_lst)
