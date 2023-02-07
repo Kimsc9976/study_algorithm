3b82c818cf9b1638cbdcd6a2a5949f210198464c
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-02-07 10:26:33 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 36912 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,70 +1,32 @@
 from collections import deque
 N, K = map(int,input().split())
-
-que = deque()
-cnt = 0
-# def bfs(start,depth):
-#   global cnt
-#   deq.append((start, depth))
-  
-#   while len(deq):
-#     now, dep = deq.popleft()
-    
-#     l = now - 1
-#     deq.append((l,dep+1))
-#     r = now + 1
-#     deq.append((r,dep+1))
-#     t = now * 2
-#     deq.append((t,dep+1))
-    
-#     if l == K or r == K or t == K:
-#       cnt = dep + 1
-#       break
-#       break
 dx = [1, -1, 2]
+que = deque()
 
-arr = [0] * (10**6 + 1)
-def bfs(start, target):
-  global arr
+def bfs(start,depth):
   
-  isVisited = [False] * (10**6 + 1)
+  isVisited = [False] * (2*10**5 + 1)
+  que.append((start, depth))
   
-  que.append(start)
-  isVisited[start] = True
-  arr[start] = 0
-  
-  while que:
-    now = que.popleft()
-    now_time = arr[now]
+  while len(que):
+    now, dep = que.popleft()
     
-    if now == target:
-      return now_time
-    
+    if now == K:
+      return dep
+      
     for t in range(3):
       if t == 2:
         nx = now * 2
-        if nx >= 2*100000:
-          continue
-        if isVisited[nx]:
-          continue
-        if nx < -1:
-          continue
-        que.append(nx)
-        isVisited[nx] = True
-        arr[nx] = now_time + 1
       else:
         nx = now + dx[t]
-        if nx >= 2*100000:
-          continue
-        if nx < -1:
-          continue
-        if isVisited[nx]:
-          continue
-        que.append(nx)
-        isVisited[nx] = True
-        arr[nx] = now_time + 1
-      
-  
-
-answer = bfs(N, K)
+        
+      if nx >= 2*100000:
+        continue
+      if isVisited[nx]: # 앞에 더욱 먼저 이곳에 도착한 친구들이 있음.
+        continue
+      if nx < -1:
+        continue
+      que.append((nx,dep+1))
+      isVisited[nx] = True 
+answer = bfs(N, 0)
 print(answer)
