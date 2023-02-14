03e51d77b6c282fdd8d016d458b27f22400ed45f
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-02-14 13:53:42 +0900

    [Gold II] Title: 피보나치 수 3, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,21 @@
+import sys
+
+N = int(sys.stdin.readline())
+A = [[1, 1], [1, 0]]
+
+def pw_m(m1 : list, m2 : list):
+  result = [[0]*2 for _ in range(2)]
+  for i in range(2):
+    for j in range(2):
+      for k in range(2):
+        result[i][j] += (m1[i][k] * m2[k][j]) % 1000000
+  return result
+
+
+def dac(num : int ,matrix : list):
+  if num == 1: return matrix
+  mat = dac(num // 2, matrix)
+  return pw_m(mat, mat) if num%2 ==0 else pw_m(pw_m(mat, mat),matrix)
+
+ans = dac(N, A)
+print(ans[0][1]%1000000) 
