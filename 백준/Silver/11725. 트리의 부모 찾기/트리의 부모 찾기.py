62e6990ac29712dda656541065659c8a1a60c7c6
Kimsc9976 <tncks097@naver.com>
2023-02-28 09:20:30 +0900

    Feat : 주석 추가

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -6,13 +6,18 @@ def bfs(start):
     que.append(start)
     while que:
         now = que.popleft()
+        if is_visited[now] : continue   # 두번 연산 되는 것을 계속 까먹음;; -> 이렇게 해서 방지
         is_visited[now] = True
         for item in nodes[now]:
             if is_visited[item] == True: continue
-            parent[item] = now
+            parent[item] = now ## 각노드들의 부모노드를 저장해주면 
             que.append(item)
 
-
+## 이번문제는 탐색 문제로 Root 노드를 1이라 가정을 하였을 때 각 노드들에 대한 
+## 부모노드를 찾는 문제
+## 1번 노드를 기준으로 탐색을 시작을 해서 is_visited를 활용하여 이전에 탐색한
+## 노드들을 지우게 되면 이것은 부모를 탐색한 것과 같은 것이다.
+## 따라서 탐색을 진행하고 나서 -- 해주면, 결과를 출력해 줄 수 있다. 
 N = int(input())
 
 is_visited = [False for _ in range(N+1)]
