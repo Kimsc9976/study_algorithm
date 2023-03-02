d4c0dcc3444a3ef6b98f7a20819f08512e0a9267
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-03-02 16:47:02 +0900

    [Gold IV] Title: 공유기 설치, Time: 280 ms, Memory: 118224 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,32 @@
+N,C = map(int,input().split())
+
+
+arr = [int(input()) for _ in range(N)]
+arr.sort()
+is_travel = [False for _ in range(N)]
+
+
+start = 1
+end = arr[N - 1]
+length = end - arr[0]
+
+if C == 2:
+    print(length)
+else:
+    ans = length
+    while start <= end:
+        mid = (start + end) // 2
+        curr = arr[0]
+        count = 1
+
+        for i in range(N):
+            if (arr[i] - curr) >= mid:
+                count += 1
+                curr = arr[i]
+
+        if count >= C:
+            start = mid + 1
+            ans = mid
+        else: # count < C
+            end = mid - 1
+    print(ans)
