9365c1ae9af82afdcb15f71314316a7f62cdf758
Kimsc9976 <tncks097@naver.com>
2023-03-28 23:31:26 +0900

    주석

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -4,7 +4,7 @@ eggs = [list(map(int,input().split())) for _ in range(N)]
 
 ans = 0
 
-def check():
+def check(): # 부서진 계란을 파악한다
     cnt = 0
     for egg in eggs:
         if egg[0] <= 0:
@@ -14,13 +14,15 @@ def check():
 
 def dfs(now):
     global ans
-    if now == N:
-        ans = max(ans, check())
+    if now == N: # 마지막 계란을 잡을 경우 stop
+        ans = max(ans, check()) # 부서진 계란의 max값을 찾아본다
         return
     
+    
+    # 아래는 계란을 부수는 작업을 진행
     stiff, weight = eggs[now]
     if stiff<=0:
-        dfs(now+1)
+        dfs(now+1) # 현재 계란이 부셔져있을경우 다음계란으로
     else:
         is_all_broken = True
         for i in range(N):
@@ -28,7 +30,7 @@ def dfs(now):
                 is_all_broken = False
                 eggs[now][0] = stiff - eggs[i][1]
                 eggs[i][0] = eggs[i][0] - weight
-                dfs(now + 1)
+                dfs(now + 1) # 계란을 계속 부수기
                 eggs[now][0] = stiff
                 eggs[i][0] = eggs[i][0] + weight
         if is_all_broken:
