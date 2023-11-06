07556fe03f1d20591c1a083d5560337a045636d8
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-11-06 22:58:43 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,24 +1,18 @@
-from itertools import combinations
-
-def dfs(now, is_visited, cards, counts):
-    if is_visited[now]: return counts
-    
-    is_visited[now] = True
-    _next = cards[now]
-    
-    return dfs(_next, is_visited, cards, counts+1)
-
 def solution(cards):
-    answer = 0
-    cards = [0] + cards
+    length = len(cards)
+    is_visited = [False for _ in range(length)]
+    box = list()
     
+    for i in range(length) :
+        if not is_visited[i] :
+            idx = i
+            cnt = 0
+            while not is_visited[idx] :
+                cnt += 1
+                is_visited[idx] = True
+                idx = cards[idx] - 1
+            box.append(cnt)
+    box.sort(reverse = True)
+    box.append(0)
     
-    for a, b in combinations(range(1, len(cards)), 2):
-        is_visited = [False for _ in range(len(cards))]
-        
-        count1 = dfs(a, is_visited, cards, 0)
-        count2 = dfs(b, is_visited, cards, 0)
-        
-        answer = max(answer, count1*count2)
-    
-    return answer
+    return box[0]*box[1]
