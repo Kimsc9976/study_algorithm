21d215947654d74e160bafc85c49b4f81e305eae
Kimsc9976 <68680106+Kimsc9976@users.noreply.github.com>
2023-11-27 22:49:04 +0900

    Update 간단한 DP를 활용한 성능향상.java

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -2,12 +2,29 @@ class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
         
-        for(int i = 0; i < n; i ++){
-            System.out.println(x);
-            answer[i] = (long)(i+1) * x;
+        answer[0] = (long) x;
+        
+        for(int i = 1; i < n; i ++){
+            answer[i] = answer[i-1] + x;
 
         }
         
         return answer;
     }
 }
+
+
+// class Solution {
+//     public long[] solution(int x, int n) {
+//         long[] answer = new long[n];
+        
+//         for(int i = 0; i < n; i ++){
+//             System.out.println(x);
+//             answer[i] = (long)(i+1) * x;
+
+//         }
+        
+//         return answer;
+//     }
+// }
+
