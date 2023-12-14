dbc084879a3b46e9505eaee82df2f176b2a58996
Kimsc9976 <tncks097@naver.com>
2023-12-15 00:12:46 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int whole = brown + yellow;
-        int width = (int)Math.sqrt(whole);
-        int height = width;
 
-        while (width * height != whole || (width - 2) * (height - 2) != yellow) {
-            if (width * height > whole) {
-                height--;
-            } else {
-                width++;
+        for (int width = whole; width >= 3; width--) {
+            if (whole % width == 0) { // 세로 길이 계산 가능
+                int height = whole / width;
+                if ((width - 2) * (height - 2) == yellow) {
+                    return new int[]{width, height};
+                }
             }
         }
-
-        return new int[]{width, height};
+        return new int[]{0, 0}; // 정답을 찾지 못한 경우
     }
 }
