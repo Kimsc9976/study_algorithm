c9b4018fca130ba5e361767c20c67c1c80cc121f
Kimsc9976 <tncks097@naver.com>
2024-01-09 23:15:07 +0900

    [Gold V] Title: 동전 2, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+
+dp = [10001] * (k+1)
+dp[0] = 0
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] = min(dp[i],dp[i-coin]+1)
+
+print(dp[k] if dp[k] != 10001 else -1)
