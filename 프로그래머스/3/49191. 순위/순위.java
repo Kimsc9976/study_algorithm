07228e82fb62638eba5220e1dda4d270c7a70698
Kimsc9976 <tncks097@naver.com>
2024-02-22 23:59:43 +0900

    [level 3] Title: 순위, Time: 15.99 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+   
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        int[][] floyd = new int[n+1][n+1];
+        
+        for(int i = 0; i < results.length; i++){
+            int A = results[i][0];
+            int B = results[i][1];
+
+            floyd[A][B] = 1; 
+            floyd[B][A] = -1; 
+        }
+        
+        // a와 b와의 전적, b와 c와의 전적을 비교해서 실력차를 알아본다.
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                for(int k = 1; k <= n; k++){
+                    if(floyd[i][k] == 1 && floyd[k][j] == 1){
+                        floyd[i][j] = 1;
+                        floyd[j][i] = -1;
+                    }
+                    if(floyd[i][k] == -1 && floyd[k][j] == -1){
+                        floyd[i][j] = -1;
+                        floyd[j][i] = 1;
+                    }
+                }
+            }
+        }
+        
+        for(int i = 1; i <= n; i++){
+            int cnt = 0; 
+            for(int j = 1; j <= n; j++){
+                if(floyd[i][j] != 0) cnt++;
+            }
+            if(cnt == n-1) answer++;
+        }
+        
+        return answer;
+        
+    }
+}
