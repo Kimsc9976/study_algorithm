a6d5deaf26775e34902f9e026fa22e9a4056487e
Kimsc9976 <tncks097@naver.com>
2024-03-14 21:15:14 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -0,0 +1,10 @@
+def solution(targets):
+    answer = 0
+    targets.sort(key=lambda x: (x[1], x[0]))
+    
+    end = 0
+    for s, e in targets:
+        if s >= end:
+            answer += 1
+            end = e
+    return answer
