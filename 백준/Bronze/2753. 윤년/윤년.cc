ee8948992e7e3be182bceff3b72618b9a5e762bb
Kimsc9976 <tncks097@naver.com>
2024-08-27 21:56:38 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int year;
+    scanf("%d", &year);
+       
+    if ((year % 4 == 0) && (year % 100 != 0))
+    {printf("%d", 1);}
+    
+    else if(year % 100 == 0)
+    {
+        if(year % 400 == 0)
+        {printf("%d", 1);}
+        else{printf("%d", 0);}
+    }
+    else{printf("%d", 0);}
+
+
+    return 0;
+}
